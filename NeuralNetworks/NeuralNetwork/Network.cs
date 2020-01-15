54dc0a05bbd94ac15e17ffc169b82e8d28bb839d
Lucas Lebihan <htfilia@gmail.com>
2020-01-15 23:28:18 +0100

    Squelette prof

---
+++ NeuralNetworks/NeuralNetwork/Network.cs
@@ -0,0 +1,34 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using NeuralNetwork.Common;
+using NeuralNetwork.Common.Layers;
+using System;
+
+namespace NeuralNetwork
+{
+    public sealed class Network : IEquatable<Network>, INetwork
+    {
+        public int BatchSize { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Matrix<double> Output => throw new NotImplementedException();
+
+        public ILayer[] Layers => throw new NotImplementedException();
+
+        public Mode Mode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+
+        public bool Equals(Network other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Learn(Matrix<double> outputLayerError)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Propagate(Matrix<double> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
