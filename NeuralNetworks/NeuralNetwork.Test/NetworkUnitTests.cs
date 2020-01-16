d954de077fcc7a2a68d09b8321d0194744ecebcf
Lebihan Lucas <htfilia@gmail.com>
2020-01-16 12:15:08 +0100

    Avec le fichier test c'est mieux

---
+++ NeuralNetworks/NeuralNetwork.Test/NetworkUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NeuralNetwork.Activators;
+using NeuralNetwork.Layers;
+using MathNet.Numerics.LinearAlgebra;
+using NUnit.Framework;
+
+namespace NeuralNetwork.Tests
+{
+    public class NetworkUnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void PropagationTest()
+        {
+            Network network = new Network(1, 1, 1, new int[] { 1 }, new ActivatorReLU());
+            network.Propagate(Matrix<double>.Build.Random(1, 1));
+            Assert.NotZero(network.Output[0, 0]);
+        }
+    }
+}
