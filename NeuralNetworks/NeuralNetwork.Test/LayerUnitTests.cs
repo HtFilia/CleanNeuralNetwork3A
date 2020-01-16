f0e1476d4a96c7252aa898c7c646a2de04f4c69c
Lebihan Lucas <htfilia@gmail.com>
2020-01-16 10:56:33 +0100

    Test unitaire sur la propagation au niveau du layer

---
+++ NeuralNetworks/NeuralNetwork.Test/LayerUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NeuralNetwork.Activators;
+using NeuralNetwork.Layers;
+using MathNet.Numerics.LinearAlgebra;
+using NUnit.Framework;
+
+namespace NeuralNetwork.Tests
+{
+    public class LayerUnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void PropagationTest()
+        {
+            // We create a basic layer containing 1 neuron and 1 random input
+            StandardLayer testLayer = new StandardLayer(1, 1, 1, new ActivatorIdentity());
+            Matrix<double> testInput = Matrix<double>.Build.Random(1, 1);
+
+            // We propagate the input to this new layer
+            testLayer.Propagate(testInput);
+
+            // If all is well the new activation is not zero anymore
+            Assert.NotZero(testLayer.Activation[0, 0]);
+        }
+    }
+}
