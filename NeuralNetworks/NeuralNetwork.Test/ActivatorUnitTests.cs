9be9989a0c7b9288a526feecefc3b7aafce28822
Lebihan Lucas <htfilia@gmail.com>
2020-01-16 10:26:51 +0100

    Les tests sont OK pour activators

--- NeuralNetworks/NeuralNetwork.Test/ActivatorUnitTests.cs
+++ NeuralNetworks/NeuralNetwork.Test/ActivatorUnitTests.cs
@@ -16,15 +16,15 @@ namespace NeuralNetwork.Test
         {
             ActivatorIdentity activatorIdentity = new ActivatorIdentity();
             Assert.AreEqual(0.2, activatorIdentity.Apply(0.2), 0.01);
-            Assert.AreEqual(1, activatorIdentity.Apply(0.2), 0.001);
+            Assert.AreEqual(1, activatorIdentity.ApplyDerivative(0.2), 0.001);
         }
 
         [Test]
         public void SigmoidTest()
         {
             ActivatorSigmoid activatorSigmoid = new ActivatorSigmoid();
-            Assert.AreEqual(2.1913, activatorSigmoid.Apply(0.2), 0.001);
-            Assert.AreEqual(2.1913 * (1 - 2.1913), activatorSigmoid.Apply(0.2), 0.001);
+            Assert.AreEqual(0.5498, activatorSigmoid.Apply(0.2), 0.001);
+            Assert.AreEqual(0.5498 * (1 - 0.5498), activatorSigmoid.ApplyDerivative(0.2), 0.001);
         }
 
         [Test]
@@ -32,9 +32,9 @@ namespace NeuralNetwork.Test
         {
             ActivatorReLU activatorReLU = new ActivatorReLU();
             Assert.AreEqual(0.2, activatorReLU.Apply(0.2), 0.001);
-            Assert.AreEqual(1, activatorReLU.Apply(0.2), 0.001);
+            Assert.AreEqual(1, activatorReLU.ApplyDerivative(0.2), 0.001);
             Assert.AreEqual(0, activatorReLU.Apply(-0.2), 0.001);
-            Assert.AreEqual(0, activatorReLU.Apply(-0.2), 0.001);
+            Assert.AreEqual(0, activatorReLU.ApplyDerivative(-0.2), 0.001);
         }
     }
 }
