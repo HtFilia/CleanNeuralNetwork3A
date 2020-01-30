7ff5419205e519a7e1891bfc34b77629ecc11678
mouad elmed <suppramouad@gmail.com>
2020-01-30 15:20:40 +0100

    pricing network project

---
+++ PricingNetwork/PricingNetwork/NetworkCreator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuralNetwork;
+using NeuralNetwork.Activators;
+using NeuralNetwork.Common.GradientAdjustmentsParameters;
+using NeuralNetwork.Common.Layers;
+using NeuralNetwork.Common.Serialization;
+using NeuralNetwork.Layers;
+using NeuralNetwork.Serialization;
+using Newtonsoft.Json;
+
+namespace PricingNetwork
+{
+    class NetworkCreator
+    {
+        static void Main()
+        {
+                StandardLayer hiddenLayer = new StandardLayer(5,
+                    7, 5, new AdamParameters(0.01, 0.9, 0.999, 1E-8), new ActivatorLeakyReLU());
+                StandardLayer outputLayer = new StandardLayer(1, 5, 7,
+                    new AdamParameters(0.01, 0.9, 0.999, 1E-8), new ActivatorIdentity());
+
+                Network princingNetwork = new Network(7, new ILayer[] { hiddenLayer, outputLayer });
+
+            SerializedNetwork serializedNetwork = NetworkSerializer.Serialize(princingNetwork);
+
+            try
+            {
+                using(FileStream networkJson = File.Create("./pricingNetwork.json"))
+                {
+                    byte[] jsonText = new UTF8Encoding(true).GetBytes(JsonConvert.SerializeObject(serializedNetwork));
+                    networkJson.Write(jsonText, 0, jsonText.Length);
+                    networkJson.Close();
+                }
+            } catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
